55e3ae00374488b262e33e986f05e11787b4c249
yy <yy@qq.com>
2019-11-23 23:28:38 +0800

    init repo

---
+++ src/test/java/cn/yy/elastic/ElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yy.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
